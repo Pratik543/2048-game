b1a0802d4b467cd98e39752c7e67aa17e0c7cad0
Pratik <71585947+Pratik543@users.noreply.github.com>
2026-03-04 00:16:54 +0530

    updated user_data

--- terraform-setup/script.sh
+++ terraform-setup/script.sh
@@ -3,9 +3,10 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update -y
-apt-get install -y curl unzip gnupg software-properties-common
+sudo apt-get update -y
+sudo apt-get install -y curl unzip gnupg software-properties-common
 
-apt-get install -y docker.io
-systemctl enable --now docker
-systemctl start docker
+sudo apt-get install -y docker.io
+sudo systemctl enable --now docker
+sudo systemctl start docker
+sudo usermod -aG docker ubuntu
